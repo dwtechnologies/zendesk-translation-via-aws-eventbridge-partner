5294c8e4035c4edfe5bcf51bdc0eff194047acc8
Lezgin Bakircioglu <lezgin.bakircioglu@danielwellington.com>
2020-11-27 14:18:10 +0100

    Added more up-to-date events/tests

--- lambda/mock.js
+++ lambda/mock.js
@@ -1,5 +1,5 @@
-const agentIntNoteEvent = require('../events/agent_internal_note.json');
-const custCommEvent = require('../events/customer_comment.json');
+const agentIntNoteEvent = require('../test/lambda-test-zendesk-internal-note.json);
+const custCommEvent = require('../test/lambda-test-zendesk-customer-public.json');
 const AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('../config/credentials.json');
---
+++ test/Testing.md
@@ -0,0 +1,1 @@
+You can use this example to create a test for lambda to try without eventbridge for troubleshooting purposes
